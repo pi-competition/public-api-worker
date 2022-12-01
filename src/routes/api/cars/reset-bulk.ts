8a0d77a474ce9298140b3e092d5194c7e57bf9b8
Ben754444 <50147798+Ben754444@users.noreply.github.com>
2022-12-01 11:18:03 +0000

    require auth for post requests

--- src/routes/api/cars/reset-bulk.ts
+++ src/routes/api/cars/reset-bulk.ts
@@ -2,6 +2,7 @@ import {Env} from "../../../index";
 import {error, success} from "../../../utils/utils";
 import {validate} from "../../../utils/body";
 import {getStatuses} from "./status";
+import { RemoteConfig } from "../../../interfaces/RemoteConfig";
 
 const schema = {
     ids: []
@@ -15,8 +16,12 @@ type Schema = {
 export default async function execute(
     request: Request,
     env: Env,
-    ctx: ExecutionContext
+    ctx: ExecutionContext & RemoteConfig.AdditionalContext
 ): Promise<Response> {
+    const auth = request.headers.get("Authorization");
+    if (!auth) return error(401, "You must provide authentication credentials to access this resource.");
+    if (auth !== "admin") return error(403, "You do not have permission to access this resource.");
+
     let content: Schema;
     try {
         content = await request.json();
